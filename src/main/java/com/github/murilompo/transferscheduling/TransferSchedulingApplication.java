1e2fa6834831a7c355bf21a0e656dbc5d816af35
Murilo M. P. Oliveira <murilo_mpo@hotmail.com>
2021-09-14 08:03:23 -0300

    base structure

---
+++ src/main/java/com/github/murilompo/transferscheduling/TransferSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.github.murilompo.transferscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/murilompo/transferscheduling/TransferSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.murilompo.transferscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
